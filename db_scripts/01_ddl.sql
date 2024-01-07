e19553cbe70e873fa82936e4e0bd8d6cdd410dcd
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2024-01-07 22:08:03 +0500

    upd 01_ddl.sql

--- db_scripts/01_ddl.sql
+++ db_scripts/01_ddl.sql
@@ -4,12 +4,12 @@ create table employees(
 	first_name				text not null,
 	father_name 			text not null,
 	snils					integer unique not null,
-	tin						integer unique not null,
+	tin						bigint unique not null,
 	passport_sn				integer unique not null,
 	passport_num 			integer unique not null,
 	insurance_qty_after		interval not null,
 	onboarding_date			date not null,
-	leaving_date			date not null,
+	leaving_date			date,
 	insurance_start_date	date not null,
 	primary key(emp_id),
 	constraint uniq_passport_sn_passport_num unique(passport_sn, passport_num)	
@@ -34,7 +34,7 @@ create table emp_jobs_data (
 create table emp_salary_data(	
 	emp_id				integer not null,
 	salary_start_date	date not null,
-	salary				Numeric(2,2) not null,
+	salary				Numeric(7,2) not null,
 	primary key(emp_id,salary),
 	constraint uniq_emp_salary_data unique(emp_id,salary,salary_start_date),
 	foreign key(emp_id) references employees(emp_id)
@@ -47,6 +47,7 @@ create table emp_sick_data (
 	date_stop_sick		date not null,
 	primary key(emp_id),
 	constraint uniq_emp_sick_data unique(emp_id,date_start_sick,date_stop_sick),
+	check ( date_stop_sick > date_start_sick),
 	foreign key(emp_id) references employees(emp_id)
 );
 
@@ -59,6 +60,7 @@ create table emp_piecework_data (
 	payment_date		date not null,
 	primary key(emp_id),
 	constraint uniq_emp_piecework_data unique(emp_id,contract_number),
+	check ( expiration_date > contract_date),
 	foreign key(emp_id) references employees(emp_id)
 
 );
