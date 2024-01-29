87e8e3b8affcba970801d39491e9a007ff12d8d0
Sergey Ermachkov <ermachkovsavictory12@gmail.com>
2024-01-29 12:50:47 +0500

    complete view for payroll_sheet

--- app/controllers/payroll_sheet_controller.rb
+++ app/controllers/payroll_sheet_controller.rb
@@ -1,7 +1,12 @@
 class PayrollSheetController < ApplicationController
 
   def index
+    @year = 2023
+    @month = 12
+    
     @payroll_sheet = PayrollSheet.
-    find_by_sql(["select * from get_payroll_sheet(?,?)",12,2023])
+    find_by_sql(["select * from get_payroll_sheet(?,?)",@month,@year])
+
+    
   end
 end
